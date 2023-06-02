86f44032097bffc4dbeab3e6e778d97cf8a3cbe2
AMAN MUNJEWAR <munjewaraman07@gmail.com>
2023-06-03 01:21:42 +0530

    Please don't create empty files. It creates issues.

--- lib/pages/menu_pages/new_hostelite_page.dart
+++ lib/pages/menu_pages/new_hostelite_page.dart
@@ -1,1 +1,15 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class newHostel extends StatefulWidget {
+  const newHostel({Key? key}) : super(key: key);
+
+  @override
+  State<newHostel> createState() => _newHostelState();
+}
+
+class _newHostelState extends State<newHostel> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
