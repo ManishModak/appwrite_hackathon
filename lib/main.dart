6c2dd9e32fea2d00dc8f22c5e6c0c157b34f9390
Manish <manishmodak90@gmail.com>
2023-06-03 09:22:22 +0530

    update project

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mat_security/pages/authenticationPages//login_page.dart';
 import 'package:mat_security/pages/menuPages//manualentry_page.dart';
-import 'package:mat_security/pages/menuPages/newAdmin_page.dart';
+import 'package:mat_security/pages/menuPages/newadmin_page.dart';
 import 'package:mat_security/pages/menu_page.dart';
 import 'package:mat_security/pages/menuPages/newstudent_page.dart';
-import 'package:mat_security/pages/menuPages/dailyLog_page.dart';
+import 'package:mat_security/pages/menuPages/dailylog_page.dart';
 
 void main() async {
 
