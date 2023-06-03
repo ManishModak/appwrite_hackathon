a2a4b7d724c6b97e7ab2aeddb8a260fd6c343f97
fadkeabhi <31249309+fadkeabhi@users.noreply.github.com>
2023-06-03 18:28:55 +0530

    in out log completed

--- lib/services/log_database.dart
+++ lib/services/log_database.dart
@@ -7,7 +7,10 @@ class LogDatabase {
 
   late final Client _client;
   late final Databases _database;
-  String dID = '647ad1a8c6147fd612a1';
+  String logsDId = '647ad1a8c6147fd612a1';
+  String logsCollId = '647ad4ee0dffdf01fdae';
+  String userDId = '647a0fa72e02c1bdcc70';
+  String studCollId = '647ad7cd03478b4f9497';
 
   LogDatabase() {
     _client = Client().setEndpoint('https://cloud.appwrite.io/v1').setProject('6479bcbb10618eda232a');
@@ -19,48 +22,119 @@ class LogDatabase {
     String formattedTime = DateFormat('HH:mm:ss').format(currentTime);
     String formattedDate = DateFormat('yyyyMMdd').format(currentTime);
 
-    final snapshot = await _database.listDocuments(
-      collectionId: 'logs_$formattedDate',
-      databaseId: dID,
-    );
+    //check if id exists in database
 
-    for (var doc in snapshot.documents) {
-      if (doc.data['id'] == id && doc.data['inTime'] == null) {
-        await _database.updateDocument(
-          collectionId: 'logs_$formattedDate',
-          databaseId: dID,
-          documentId: id,
-          data: {'inTime': formattedTime},
-        );
-        return;
-      }
+    try{
+      final stud = await _database.getDocument(databaseId: userDId, collectionId: studCollId, documentId: id);
+      print('stud found');
+    }
+    catch (e){
+      print('Stud not fount $e');
+      return;
     }
 
-    MainDatabase student = MainDatabase();
-
-    Map<String, dynamic> data = await student.getInfo(id: id);
-
-    Map<String, dynamic> stdData = {
-      'id': id,
-      'roomNo': data['roomNo'],
-      'name': data['name'],
-      'outTime': formattedTime,
-      'inTime': null,
-    };
-
-    try {
-      await _database.createDocument(
-        collectionId: 'logs_$formattedDate',
-        databaseId: dID,
-        documentId: DateTime.now().toString(),
-        data: stdData,
+    //if student found check if student is gone out
+    try{
+      final result = await _database.listDocuments(
+          databaseId: logsDId,
+          collectionId: logsCollId,
+          queries : [Query.equal('id', id), Query.orderDesc('\$createdAt'), Query.limit(1)]
       );
-
-      if (kDebugMode) {
-        print('done');
+      bool isStudOut;
+      if(result.total == 0){
+        //no out entry
+        print('student is in.no out entry');
+        isStudOut = false;
       }
-    } catch (e) {
-      print('Error adding log: $e');
+      else{
+        if(result.documents[0].data['inTime'] == 'null'){
+          print('student is still out');
+          isStudOut = true;
+        }
+        else{
+          print('student is returned');
+          isStudOut = false;
+        }
+      }
+      // result.documents[0].data.forEach((key, value) {
+      //   print("$key -> $value");});
+      // print(result.documents[0].data);
+
+      //update record if student is out
+      if(isStudOut){
+        await _database.updateDocument(databaseId: logsDId,
+            collectionId: logsCollId,
+            documentId: result.documents[0].$id,
+            data : {'inTime':formattedTime}
+        );
+        print('updated record');
+      }
+      else{
+        //create new record in log
+        var formattedId = formattedDate + formattedTime.replaceAll(':', '');
+        await _database.createDocument(databaseId: logsDId,
+            collectionId: logsCollId,
+            documentId: formattedId,
+            data : {
+          'id':id,
+          'date':formattedDate,
+          'outTime':formattedTime,
+          'inTime':'null'
+        }
+        );
+        print('created record');
+      }
+
+    }catch (e){
+      print(e);
     }
+
+
+
+
+    //
+    // final snapshot = await _database.listDocuments(
+    //   collectionId: 'logs_$formattedDate',
+    //   databaseId: dID,
+    // );
+    //
+    // for (var doc in snapshot.documents) {
+    //   if (doc.data['id'] == id && doc.data['inTime'] == null) {
+    //     await _database.updateDocument(
+    //       collectionId: 'logs_$formattedDate',
+    //       databaseId: dID,
+    //       documentId: id,
+    //       data: {'inTime': formattedTime},
+    //     );
+    //     return;
+    //   }
+    // }
+    //
+    // MainDatabase student = MainDatabase();
+    //
+    // Map<String, dynamic> data = await student.getInfo(id: id);
+    //
+    // Map<String, dynamic> stdData = {
+    //   'id': id,
+    //   'roomNo': data['roomNo'],
+    //   'name': data['name'],
+    //   'outTime': formattedTime,
+    //   'inTime': null,
+    // };
+    //
+    // try {
+    //   await _database.createDocument(
+    //     collectionId: 'logs_$formattedDate',
+    //     databaseId: dID,
+    //     documentId: DateTime.now().toString(),
+    //     data: stdData,
+    //   );
+    //
+    //   if (kDebugMode) {
+    //     print('done');
+    //   }
+    // } catch (e) {
+    //   print('Error adding log: $e');
+    // }
   }
 }
